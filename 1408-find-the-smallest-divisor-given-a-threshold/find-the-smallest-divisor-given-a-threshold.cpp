211c0ea4aea9af154c9568e8ab0dcf1c2acb0c09
Pranav Murthy <93529653+pmcode9992@users.noreply.github.com>
2024-05-28 10:57:13 +0530

    Time: 45 ms (5.85%) | Memory: 25.8 MB (10.88%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -13,7 +13,6 @@ public:
             for(int i =0;i<n;i++){
                 s+=ceil((double)nums[i]/(double)m);
             }
-            cout<<m<< " - "<<s<<endl;
             if(s > threshold){
                 //check right
                 l = m + 1;
