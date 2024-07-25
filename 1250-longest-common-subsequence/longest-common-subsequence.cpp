b2c5f93feaee03eaa2d24f2993567ec90e0ac115
Pranav Murthy <93529653+pmcode9992@users.noreply.github.com>
2024-07-25 10:42:48 +0530

    Time: 44 ms (15.77%) | Memory: 26.2 MB (5.30%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -4,7 +4,7 @@ public:
         if (dp[i][j] != -1) {
             return dp[i][j];
         }
-        if( i == 0 || j == 0){return dp[i][j] = 0;}
+        // if( i == 0 || j == 0){return dp[i][j] = 0;}
         
         if (text1[i - 1] == text2[j - 1]) {
             return dp[i][j] = 1 + f(i - 1, j - 1, text1, text2, dp);
@@ -17,12 +17,12 @@ public:
         int n = text1.length();
         int m = text2.length();
         vector<vector<int>> dp(n + 1, vector<int>( m + 1, -1));
-        // for (int i = 0; i <= n; i++) {
-        //     dp[i][0] = 0;
-        // }
-        // for (int i = 0; i <= m; i++) {
-        //     dp[0][m] = 0;
-        // }
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[0][i] = 0;
+        }
         f(n, m, text1, text2, dp);
         return dp[n][m];
     }
