7cb5ee70423c44d02ce2955553ab22e98a4176e4
Pranav Murthy <93529653+pmcode9992@users.noreply.github.com>
2024-08-10 19:09:30 +0530

    Time: 9 ms (49.69%) | Memory: 12.7 MB (45.79%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,31 +1,51 @@
 class Solution {
 public:
-    int f(int i, int j , string word1, string word2, vector<vector<int>> &dp){
-        if(i == 0){
-            return j;
-        }
-        if(j == 0){
-            return i;
-        }
-        if(dp[i][j] !=-1){
-            return dp[i][j];
-        }
-        if(word1[i-1] == word2[j-1]){
-            //match
-            return dp[i][j] = f(i-1, j-1, word1, word2, dp);
-        }
-        else{
-            //no match
-            int rep = 1 + f(i-1, j-1, word1, word2, dp);
-            int del = 1 + f(i-1, j, word1, word2, dp);
-            int ins = 1 + f(i,j-1, word1, word2, dp);
-            return dp[i][j] = min(rep, min(del, ins));
-        }
-    }
+    // int f(int i, int j, string word1, string word2, vector<vector<int>>& dp) {
+    //     if (i == 0) {
+    //         return j;
+    //     }
+    //     if (j == 0) {
+    //         return i;
+    //     }
+    //     if (dp[i][j] != -1) {
+    //         return dp[i][j];
+    //     }
+    //     if (word1[i - 1] == word2[j - 1]) {
+    //         // match
+    //         return dp[i][j] = f(i - 1, j - 1, word1, word2, dp);
+    //     } else {
+    //         // no match
+    //         int rep = 1 + f(i - 1, j - 1, word1, word2, dp);
+    //         int del = 1 + f(i - 1, j, word1, word2, dp);
+    //         int ins = 1 + f(i, j - 1, word1, word2, dp);
+    //         return dp[i][j] = min(rep, min(del, ins));
+    //     }
+    // }
     int minDistance(string word1, string word2) {
         int n = word1.length();
         int m = word2.length();
-        vector<vector<int>> dp(n + 1, vector<int> (m+1, -1));
-        return f(word1.length(), word2.length(), word1, word2, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for(int j = 0;j<=m;j++){
+            dp[0][j] = j;
+        }
+        for(int i = 0;i<=n;i++){
+            dp[i][0] = i;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    // match
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    // no match
+                    int rep = 1 + dp[i - 1][j - 1];
+                    int del = 1 + dp[i - 1][j];
+                    int ins = 1 + dp[i][j - 1];
+                    dp[i][j] = min(rep, min(del, ins));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
