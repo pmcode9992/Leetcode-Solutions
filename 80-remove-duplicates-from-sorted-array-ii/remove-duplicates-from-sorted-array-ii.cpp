8dce87c6d9a266e7b44c10a1caa75f1d90262088
Pranav Murthy <93529653+pmcode9992@users.noreply.github.com>
2025-04-25 15:14:31 +0530

    Time: 6 ms (75.31%) | Memory: 19.5 MB (56.73%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int max = nums[nums.size()-1];
-        int k=0;
-        bool match = false;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]&& match == false){
-                match = true;
-                k++;
-            }
-            else if(nums[i] == nums[i+1]&& match == true){
-                nums[i] = max+1;
+        int slow = 0;
+        int freq = 0;
+        int fast = 1;
+        for(fast = 1; fast < nums.size() ; fast++){
+            if(nums[slow] == nums[fast]){
+                freq++;
             }
             else{
-                k++;
-                match = false;
+                if(freq){
+                    nums[slow + 1] = nums[slow];
+                    nums[slow + 2] = nums[fast];
+                    slow += 2;
+                }
+                else{
+                    nums[slow + 1] = nums[fast];
+                    slow++;
+                }
+                freq = 0;
             }
+            
         }
-        sort(nums.begin() , nums.end());
-        return k+1;
-
+        if(freq){
+            nums[++slow] = nums[--fast];
+        }
+        
+        return slow + 1;
     }
 };
