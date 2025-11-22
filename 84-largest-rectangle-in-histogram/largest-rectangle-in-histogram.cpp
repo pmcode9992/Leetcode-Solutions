cf39159b8dd464184ed7e1fe10697f80096db83a
Pranav Murthy <93529653+pmcode9992@users.noreply.github.com>
2025-11-22 19:23:22 +0530

    Time: 40 ms (14.20%) | Memory: 89.5 MB (10.90%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int> stack;
-        stack.push(-1);
-        int max_area = 0;
-
-        for (int i = 0; i < heights.size(); i++) {
-            while (stack.top() != -1 && heights[i] <= heights[stack.top()]) {
-                int height = heights[stack.top()];
-                stack.pop();
-                int width = i - stack.top() - 1;
-                max_area = max(max_area, height * width);
+        stack<int> st, stl;
+        vector<int> nse(heights.size(), -1);
+        vector<int> nsel(heights.size(), -1);
+        int area = 0;
+        for(int i = 0 ; i < heights.size() ; i++){
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                nse[st.top()] = i - st.top();
+                st.pop();
             }
-            stack.push(i);
+            st.push(i);
         }
-
-        while (stack.top() != -1) {
-            int height = heights[stack.top()];
-            stack.pop();
-            int width = heights.size() - stack.top() - 1;
-            max_area = max(max_area, height * width);
+        for(int i = heights.size() - 1; i >= 0 ; i--){
+            while(!stl.empty() && heights[stl.top()] > heights[i]){
+                nsel[stl.top()] = stl.top() - i - 1;
+                stl.pop();
+            }
+            stl.push(i);
         }
-
-        return max_area;        
+        while(!stl.empty()){
+            nsel[stl.top()] = stl.top();
+            stl.pop();
+        }
+        while(!st.empty()){
+            nse[st.top()] = heights.size() - st.top();
+            st.pop();
+        }
+        for(int i = 0 ; i< heights.size() ; i++){
+            area = max((heights[i] * (nse[i] + nsel[i])), area);
+        }
+        return area;
     }
 };
