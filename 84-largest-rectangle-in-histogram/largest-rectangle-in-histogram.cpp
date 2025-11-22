66647210f8a8171bdf4ef3fd22097b71e34ae9f6
Pranav Murthy <93529653+pmcode9992@users.noreply.github.com>
2025-11-22 19:22:56 +0530

    Time: 23 ms (57.41%) | Memory: 79.4 MB (93.11%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int> st, stl;
-        vector<int> nse(heights.size(), -1);
-        vector<int> nsel(heights.size(), -1);
-        int area = 0;
-        for(int i = 0 ; i < heights.size() ; i++){
-            while(!st.empty() && heights[st.top()] > heights[i]){
-                nse[st.top()] = i - st.top();
-                st.pop();
+        stack<int> stack;
+        stack.push(-1);
+        int max_area = 0;
+
+        for (int i = 0; i < heights.size(); i++) {
+            while (stack.top() != -1 && heights[i] <= heights[stack.top()]) {
+                int height = heights[stack.top()];
+                stack.pop();
+                int width = i - stack.top() - 1;
+                max_area = max(max_area, height * width);
             }
-            st.push(i);
+            stack.push(i);
         }
-        for(int i = heights.size() - 1; i >= 0 ; i--){
-            while(!stl.empty() && heights[stl.top()] > heights[i]){
-                nsel[stl.top()] = stl.top() - i - 1;
-                stl.pop();
-            }
-            stl.push(i);
+
+        while (stack.top() != -1) {
+            int height = heights[stack.top()];
+            stack.pop();
+            int width = heights.size() - stack.top() - 1;
+            max_area = max(max_area, height * width);
         }
-        while(!stl.empty()){
-            nsel[stl.top()] = stl.top();
-            stl.pop();
-        }
-        while(!st.empty()){
-            nse[st.top()] = heights.size() - st.top();
-            st.pop();
-        }
-        for(int i = 0 ; i< heights.size() ; i++){
-            area = max((heights[i] * (nse[i] + nsel[i])), area);
-        }
-        return area;
+
+        return max_area;        
     }
 };
