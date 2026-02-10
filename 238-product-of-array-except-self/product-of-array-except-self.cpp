f7366d477e935f3acea59a33a9a6a6ad3034a4ea
Pranav Murthy <93529653+pmcode9992@users.noreply.github.com>
2026-02-10 17:03:05 +0530

    Time: 3 ms (37.73%) | Memory: 42.2 MB (25.03%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> pre(nums.size(), 0), suf(nums.size() , 0);
         int n = nums.size();
-        
-        std::vector<int> prefix(n, 1);
-        std::vector<int> suffix(n, 1);
-        
-        for (int i = 1; i < n; ++i) {
-            prefix[i] = prefix[i - 1] * nums[i - 1];
+        pre[0] = nums[0];
+        suf[n - 1] = nums[n - 1];
+        for(int i = 1; i < n ; i++){
+            pre[i] = pre[i - 1] * nums[i];
+            suf[n - i - 1] = suf[n - i] * nums[n - i - 1];
         }
-        
-        for (int i = n - 2; i >= 0; --i) {
-            suffix[i] = suffix[i + 1] * nums[i + 1];
+        for(int i = 0 ; i < n ; i++){
+            if(i == 0){
+                nums[i] = suf[1];
+            }
+            else if(i == n - 1){
+                nums[i] = pre[i - 1];
+            }
+            else{
+                nums[i] = pre[i - 1] * suf[i + 1];
+            }
         }
-        
-        std::vector<int> answer(n);
-        for (int i = 0; i < n; ++i) {
-            answer[i] = prefix[i] * suffix[i];
-        }
-        
-        return answer;
+        return nums;
+
     }
 };
