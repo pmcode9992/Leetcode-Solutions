f76401bad54c8c67669a02a38f4715e64e7cf029
Pranav Murthy <93529653+pmcode9992@users.noreply.github.com>
2026-04-15 16:40:17 +0530

    Time: 18 ms (12.53%) | Memory: 11.1 MB (7.34%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
-    bool freqCheck(map<char, int> mpp, int k){
-        char greatest = (mpp.begin())->first;
-        for(auto it= mpp.begin() ; it!=mpp.end();it++){
-            if(mpp[greatest] <= it->second){
-                greatest = it->first;
-
-            }
-        }
-        int sum =0;
-        for(auto it= mpp.begin() ; it!=mpp.end();it++){
-            if(it->first!= greatest){
-                sum += it->second;
-
-            }
-            if(sum > k){
-                return true;
-            }
-        }
-        return false;
-        
-    }
     int characterReplacement(string s, int k) {
-        map<char, int> mpp;
-        int end = 0;
-        int start = 0;
-        int maxi = 0;
-        while (end < s.length()) {
-            mpp[s[end]]++;
-            while(mpp.size() > 1 && freqCheck(mpp, k)){
-                mpp[s[start]]--;
+        int start = 0, end = 0, maxlen = 0, n = s.size();
+        unordered_map<char, int> freq;
+        pair<char, int> mostfreq = {s[0], 1};
+        while(end < n){
+            freq[s[end]]++;
+            if(mostfreq.second < freq[s[end]]){
+                mostfreq = {s[end], freq[s[end]]};
+            }
+            while(end - start  + 1 > mostfreq.second + k){
+                freq[s[start]]--;
                 start++;
             }
-            maxi = max(maxi, end - start + 1);
+            maxlen = max(maxlen, end - start + 1);
             end++;
         }
-        return maxi;
+        return maxlen;
+
     }
 };
+
