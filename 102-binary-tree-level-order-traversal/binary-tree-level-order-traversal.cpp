23952cb8c2471aa2739f7b1a8580e2fc97dbbb04
Pranav Murthy <93529653+pmcode9992@users.noreply.github.com>
2026-05-03 18:18:17 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (97.63%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -6,39 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        if(!root){
+        queue<pair<TreeNode *, int>> q;
+        vector<vector<int>> res;
+        if(root == nullptr){
             return {};
         }
-        queue<pair<TreeNode *, int>> q;
-        map<int, vector<int>> mpp;
         q.push({root, 0});
-        while (!q.empty()) {
-            int level = q.front().second;
-            int s = q.size();
-            for (int i = 0; i < s; i++) {
-                auto it = q.front();
-                TreeNode* node = it.first;
-                mpp[level].push_back(node->val);
-                if (node->left) {
-                    q.push({node->left, level + 1});
-                }
-                if (node->right) {
-                    q.push({node->right, level + 1});
-                }
-                q.pop();
+        while(!q.empty()){
+            auto temp = q.front();
+            if(temp.first->left != nullptr){
+                q.push({temp.first->left, temp.second + 1});
             }
+            if(temp.first->right != nullptr){
+                q.push({temp.first->right, temp.second + 1});
+            }
+            if(res.size() <= temp.second){
+                res.push_back({});
+            }
+            res[temp.second].push_back(temp.first->val);
+            q.pop();
         }
-        vector<vector<int>> soln;
-        for (auto it : mpp) {
-            soln.push_back(it.second);
-        }
-        return soln;
+        return res;
+        
     }
 };
