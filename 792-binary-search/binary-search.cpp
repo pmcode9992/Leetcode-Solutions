9e88b07a3dbf01c9d5d3c33b661b227381a32e00
Pranav Murthy <93529653+pmcode9992@users.noreply.github.com>
2026-05-21 20:02:37 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (7.08%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size();
-        int mid = 0;
-        if(r == 1){
-            if(nums[0] == target){
-                return 0;
+        int l = 0, r = nums.size() - 1;
+        while(l <= r){
+            int m = (l + r) / 2;
+            if(nums[m] < target){
+                l = m + 1;
+            }
+            else if(nums[m] > target){
+                r = m - 1;
             }
             else{
-                return -1;
+                return m;
             }
-        }
-        while(r - l > 1){
-            mid = (r - l) / 2 + l;
-            cout<< l<< " "<<mid<< " "<<r<<endl;
-            if(target == nums[mid]){
-                return mid;
-            }
-            else if(target > nums[mid]){
-                l = mid;
-            }
-            else{
-                r = mid;
-            }
-        }
-        if(nums[r] == target){
-            return r;
-        }
-        if(nums[l] == target){
-            return l;
         }
         return -1;
+        
     }
 };
-
