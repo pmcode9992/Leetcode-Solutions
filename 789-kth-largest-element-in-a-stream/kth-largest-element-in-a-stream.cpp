f56225729d9a80a78ad6f00b46fae61bbf75e6e6
Pranav Murthy <93529653+pmcode9992@users.noreply.github.com>
2026-06-10 09:05:49 +0530

    Time: 4 ms (82.56%) | Memory: 33 MB (61.97%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,21 +1,25 @@
 class KthLargest {
 public:
-    priority_queue<int, vector<int>, greater<int>> pq;
+    priority_queue<int, vector<int> , greater<int>> pq;
     int k;
     KthLargest(int k, vector<int>& nums) {
+        this->k = k;
         for(int i : nums){
             pq.push(i);
+            if(pq.size() > k){
+                pq.pop();
+            }
         }
-        this->k = k;
-
     }
     
     int add(int val) {
         pq.push(val);
-        while (pq.size() > k) {
+        if(pq.size() > k){
             pq.pop();
         }
+        // pq.pop();
         return pq.top();
+        
     }
 };
 
